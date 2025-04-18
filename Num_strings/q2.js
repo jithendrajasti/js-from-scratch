56a5d9d4f03618383f6b01bf72a835a495042274
jithendrajasti <jithendrajasti11@gmail.com>
2025-04-18 17:57:56 +0000

    deleted

--- Num_strings/q2.js
+++
@@ -1,8 +0,0 @@
-/*
-calculate 18% gst on iphone15 of 79,990rs and 2 apple Air pods pro 24990rs each.
-*/
- let iPhone15=79990;
- let Air_pods=24990;
- let bill_nogst=iPhone15+2*Air_pods;
- let after_gst=bill_nogst+bill_nogst*(18/100);
- console.log('bill after gst added is '+after_gst);
--- Num_strings/q3.js
+++
@@ -1,7 +0,0 @@
-/*
-to make string using all three methods
-*/
-console.log('method 1-single tick '+25);
-console.log(`method 2-using backticks ${25}`);
-console.log("method 3-using double ticks "+25);
-//i have included how concatenation is done in different methods
